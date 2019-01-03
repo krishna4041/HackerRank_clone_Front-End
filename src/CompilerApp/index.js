b255ce7b96952f2863c8c42d58fbb74c3e279669
Mouli2105 <sreekar.mouli1998@gmail.com>
2019-01-03 23:28:25 +0530

    Added BrowserRouter

--- src/CompilerApp/index.js
+++ src/CompilerApp/index.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react'
 import {Provider} from '../context'
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
 import Home from './Home'
 
 class CompilerApp extends Component {
     render() {
         return (
             <Provider>
-                <Home />
+                <BrowserRouter>
+                    <React.Fragment>
+                        <Route 
+                            exact path='/'
+                            render={() => <Redirect to='/home' />}
+                        />
+                        <Route 
+                            exact path='/home'
+                            component={Home}
+                        />
+                    </React.Fragment>
+                </BrowserRouter>
             </Provider>
         )
     }
